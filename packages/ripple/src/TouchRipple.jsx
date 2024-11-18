c432e7dc190e115407706bca3c15eb0fa5cc67da
朱鸿 <zhxhyy@qq.com>
2024-11-18 18:10:10 +0800

    replace inset

--- packages/ripple/src/TouchRipple.jsx
+++ packages/ripple/src/TouchRipple.jsx
@@ -54,7 +54,10 @@ const RippleRoot = styled('span', forwardRef)`
   pointer-events: none;
   position: absolute;
   z-index: 0;
-  inset: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `
 
 const RippleAnimated = styled(RippleAnimate, forwardRef)`
